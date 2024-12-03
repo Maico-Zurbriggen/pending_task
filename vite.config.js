7429e072a6d1c95fa7b121d15820b8624f7152e2
Maico Gabriel Zurbriggen <zurbriggenmaico8@gmail.com>
2024-12-03 17:55:31 -0300

    Probando

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,16 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/pending_task/',
-})
+export default defineConfig({ 
+  plugins: [react()], 
+  base: '/pending_task/', // Base URL para GitHub Pages 
+  build: { 
+    rollupOptions: { 
+      output: { 
+        entryFileNames: '[name].js', 
+        chunkFileNames: '[name].js', 
+        assetFileNames: '[name][extname]', 
+      }, 
+    }, 
+  }, 
+});
