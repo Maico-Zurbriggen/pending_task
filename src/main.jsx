7338fb0b399c58a129a64f89f8cc2fc9cdc3829d
Maico Gabriel Zurbriggen <zurbriggenmaico8@gmail.com>
2024-12-03 21:33:06 -0300

    Ruta cambiada en el dist

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css'
 import App from './App.jsx'
-
+//Para hacer el commit
 const basename = process.env.NODE_ENV === 'development' ? '/' : '/pending_task';
 
 createRoot(document.getElementById('root')).render(
